e979fa0d45065110ed99ec99bb7ebc03fbce229e
willpiam <you@example.com>
2018-04-29 23:23:48 -0400

    very small update to sort tester

--- sortTester.py
+++ sortTester.py
@@ -3,7 +3,7 @@ import pybrary
 print ("welcome to the pybrary sort tester.")
 print ("we will try and keep this program more organized")
 
-array = [2,1,4,3,8,4,5,6,3,7,3,7,8,9,6,0,43,987,12,43,567]
+array = [2,1,4,3,8,4,5,6,3,7,3,7,8,9,6,0,43,987,12,12,12,9876,232,12,43,567]
 
 print (" ")
 print ("Our unsorted array is as follows")
@@ -11,4 +11,10 @@ print (array)
 print (" ")
 
 print ("we will begin by bubbleing our array to a sort")
+
+array = pybrary.bubble(array)
+
+print ("our new array:")
+
+print (array)
 
